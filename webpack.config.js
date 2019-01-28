912c6a20e542c631e75cc2349920e2744971531f
Mark Lauer <allright96@gmail.com>
2019-01-29 01:41:09 +0700

    webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.([jt])sx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
