dc8cbd464d40f9b647cbbeab7dd08da1099958c1
Mark Lauer <allright96@gmail.com>
2019-01-30 20:20:38 +0700

    scss configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,6 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-import { connect } from 'react-redux';
+const App = () => <div>Hello, World!</div>;
 
-import { AppState } from './store';
-
-import { ProductListState } from './store/productList/types';
-import { fetchProducts } from './store/productList/actions';
-
-import { ShoppingCartState } from './store/shoppingCart/types';
-import { addToCart, removeFromCart } from './store/shoppingCart/actions';
-
-import { FilterState } from './store/filter/types';
-import { setFilter } from './store/filter/actions';
-
-import { SystemState } from './store/system/types';
-import { updateSession } from './store/system/actions';
-
-interface AppProps {
-    productList: ProductListState;
-    fetchProducts: typeof fetchProducts;
-    shoppingCart: ShoppingCartState;
-    addToCart: typeof addToCart;
-    removeFromCart: typeof removeFromCart;
-    filter: FilterState;
-    setFilter: typeof setFilter;
-    system: SystemState;
-    updateSession: typeof updateSession;
-}
-
-class App extends React.Component<AppProps> {
-    render(): React.ReactNode {
-        return <div>Hello, World!</div>;
-    }
-}
-
-const mapStateToProps = ({
-    productList,
-    shoppingCart,
-    filter,
-    system
-}: AppState) => ({
-    productList,
-    shoppingCart,
-    filter,
-    system
-});
-
-const mapDispatchToProps = () => ({
-    fetchProducts,
-    addToCart,
-    removeFromCart,
-    setFilter,
-    updateSession
-});
-
-export default hot(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(App)
-);
+export default hot(App);
