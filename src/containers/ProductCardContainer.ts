40f2b35ecffdd800e371dfa28ab5049a2d46f1c9
Mark Lauer <allright96@gmail.com>
2019-02-06 12:49:18 +0700

    shopping cart state mapped to ProductCard

--- src/containers/ProductCardContainer.ts
+++ src/containers/ProductCardContainer.ts
@@ -1,7 +1,17 @@
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
+import { AppState } from '../store';
 import ProductCard from '../components/ProductCard';
 import { addToCart, removeFromCart } from '../store/shoppingCart/actions';
+import { Product } from '../store/productList/types';
+
+interface OwnProps {
+    product: Product;
+}
+
+const mapState = ({ shoppingCart }: AppState, { product }: OwnProps) => ({
+    inCart: shoppingCart[product.id]
+});
 
 const mapDispatch = (dispatch: Dispatch) => ({
     onAddToCart: (productId: number) => dispatch(addToCart(productId)),
@@ -9,6 +19,6 @@ const mapDispatch = (dispatch: Dispatch) => ({
 });
 
 export default connect(
-    null,
+    mapState,
     mapDispatch
 )(ProductCard);
