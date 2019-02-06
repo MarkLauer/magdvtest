c6d4a648827556dce7ca1a177d422702df93d910
Mark Lauer <allright96@gmail.com>
2019-02-06 14:54:14 +0700

    ProductCard sm styles

--- src/containers/ProductCardContainer.ts
+++ src/containers/ProductCardContainer.ts
@@ -2,7 +2,11 @@ import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { AppState } from '../store';
 import ProductCard from '../components/ProductCard';
-import { addToCart, removeFromCart } from '../store/shoppingCart/actions';
+import {
+    addToCart,
+    removeFromCart,
+    setProductAmount
+} from '../store/shoppingCart/actions';
 import { Product } from '../store/productList/types';
 
 interface OwnProps {
@@ -10,12 +14,15 @@ interface OwnProps {
 }
 
 const mapState = ({ shoppingCart }: AppState, { product }: OwnProps) => ({
-    inCart: shoppingCart[product.id]
+    inCart: shoppingCart[product.id] || 0
 });
 
 const mapDispatch = (dispatch: Dispatch) => ({
     onAddToCart: (productId: number) => dispatch(addToCart(productId)),
-    onRemoveFromCart: (productId: number) => dispatch(removeFromCart(productId))
+    onRemoveFromCart: (productId: number) =>
+        dispatch(removeFromCart(productId)),
+    onSetProductAmount: (productId: number, amount: number) =>
+        dispatch(setProductAmount(productId, amount))
 });
 
 export default connect(
